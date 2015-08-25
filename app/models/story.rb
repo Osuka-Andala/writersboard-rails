b0999512ae2351da21bc89feab115c1cc18c7c54
Osuka-Andala <teresa.andala@gmail.com>
2015-08-25 13:17:18 +0300

    Added code to the story.rb file

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,7 @@
 class Story < ActiveRecord::Base
+  validates :title, presence: true
+  validates :writer, presence: true
+  validates :preview, presence: true
+  validates :url, presence: true, uniqueness: true
+
 end
