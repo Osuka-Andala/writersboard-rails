8d992afea8bed0068f9cb6be5540a8264bf8498c
Osuka-Andala <teresa.andala@gmail.com>
2015-08-25 12:06:09 +0300

    Added the stories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'stories/index'
+
+  get 'stories/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
